5de6a5cfba19f2c453c171891f930530d20fa14f
Bernardo <bernardsp@gmail.com>
2021-01-09 15:22:22 -0300

    packages

---
+++ SCRIPTS/Shell/gnome-tweak-tool.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo add-apt-repository universe -y
+
+sudo apt install gnome-tweak-tool -y
+
+#Launch 
+gnome-tweaks
+
+apt search gnome-shell-extension -y
+
+sudo apt install gnome-shell-extension-dash-to-panel -y
+
+
