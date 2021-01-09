8585ad4119f567aec6aec55f439688579c1f61e5
Bernardo <bernardsp@gmail.com>
2021-01-09 15:18:11 -0300

    packages

---
+++ SCRIPTS/Shell/sublime.sh
@@ -0,0 +1,6 @@
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+
+sudo apt install sublime-text
+
