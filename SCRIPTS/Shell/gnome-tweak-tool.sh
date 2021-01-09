2726ff63b1f6d6bdb55a509cf7884b271092e02c
Bernardo <bernardsp@gmail.com>
2021-01-09 15:23:39 -0300

    packages

--- SCRIPTS/Shell/gnome-tweak-tool.sh
+++ SCRIPTS/Shell/gnome-tweak-tool.sh
@@ -11,4 +11,5 @@ apt search gnome-shell-extension -y
 
 sudo apt install gnome-shell-extension-dash-to-panel -y
 
-
+#Reference
+#https://www.emidioleite.com.br/2020/04/25/como-instalar-o-tweak-tool-no-ubuntu-20-04-e-os-plugins-do-gnome-shell/
